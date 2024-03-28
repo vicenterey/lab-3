6a9edff3f14eb77f7f0012821c51abd5e5e4e65e
vicenterey <virey@miuandes.cl>
2024-03-27 21:57:31 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Defines the root path route ("/")
+  get "up" => "rails/health#show", as: :rails_health_check
+  root "static_pages#home"
+  get '/home', to: "static_pages#home"
+  get '/about', to: "static_pages#about"
+  get '/contact', to: "static_pages#contact"
+end
+
